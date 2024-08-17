85e34c26ecb89ea6570abdb4e339f21251080f9c
David Mora <davidnmora@gmail.com>
2024-08-16 17:56:36 -0700

    Get basic miles driven and us population datasets loaded in marimo

---
+++ scratch.py
@@ -0,0 +1,5 @@
+# Fill an array with each year from 1936 to 1995, inclusive
+# and print the array
+years = []
+for year in :
+    years.append(year)
---
+++ us_miles_driven_per_capita.py
@@ -0,0 +1,248 @@
+import marimo
+
+__generated_with = "0.8.0"
+app = marimo.App(width="medium", app_title="US miles driven per capital")
+
+
+@app.cell
+def __():
+    import marimo as mo
+    import pandas as pd
+    import numpy as np
+
+    import seaborn as sns
+    sns.set_theme(style="darkgrid")
+    import matplotlib.pyplot as plt
+    return mo, np, pd, plt, sns
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        """
+        # A. Total miles driven each year in the US
+        ## We'll need to pull data from 3 sources to get the maximum spread (1936 - 2023)
+
+        - [1936 - 1995 PDF report](https://www.fhwa.dot.gov/ohim/summary95/vm201a.pdf). FIELD: "All Motor Vehicles => A. Total Travel"
+        - "Report Archive 1992-2002" found on [this page under "archive"](https://www.fhwa.dot.gov/policyinformation/travel_monitoring/tvt.cfm). FIELD: "All Systems => Year"
+        - (from the same FHWA page) 2000 - May 2024 by month is available if you download the `24maytvt.xlsx => subsheet: SAVMPT`. FIELD: we can simply aggregate `vmt` column by year, excluding 2024 which is not a complete year.
+
+        ### NOTE: `vmt` ("vehicle miles traveled") is always in millions (so multiply by 1,000,000 to get actual value)
+        """
+    )
+    return
+
+
+@app.cell
+def __(pd):
+    # Ok, pasting in by hand the 1936 - 1995 report: https://www.fhwa.dot.gov/ohim/summary95/vm201a.pdf
+
+    _years_36_95 = [year for year in range(1936, 1995 + 1)]
+    # COLLAPSE ME:
+    _values_total_travel_all_motor_vehicles_1936_1995 = [
+    "252,128",
+    "270,110",
+    "271,177",
+    "285,402",
+    "302,188",
+    "333,612",
+    "268,224",
+    "208,192",
+    "212,713",
+    "250,173",
+    "340,880",
+    "370,894",
+    "397,957",
+    "424,461",
+    "458,246",
+    "491,093",
+    "513,581",
+    "544,433",
+    "561,963",
+    "605,646",
+    "631,161",
+    "645,004",
+    "664,653",
+    "700,480",
+    "718,762",
+    "737,421",
+    "766,734",
+    "805,249",
+    "846,298",
+    "887,812",
+    "925,899",
+    "964,005",
+    "1,015,869",
+    "1,061,791",
+    "1,109,724",
+    "1,178,811",
+    "1,259,786",
+    "1,313,110",
+    "1,280,544",
+    "1,327,664",
+    "1,402,380",
+    "1,467,027",
+    "1,544,704",
+    "1,529,133",
+    "1,527,295",
+    "1,555,308",
+    "1,595,010",
+    "1,652,788",
+    "1,720,269",
+    "1,774,826",
+    "1,834,872",
+    "1,921,204",
+    "2,025,962",
+    "2,096,487",
+    "2,144,362",
+    "2,172,050",
+    "2,247,151",
+    "2,296,378",
+    "2,357,588",
+    "2,422,823"
+    ]
+    _rows = [[year, int(_values_total_travel_all_motor_vehicles_1936_1995[index].replace(',', ""))] for index, year in enumerate(_years_36_95)]
+
+    by_year_1936_1995 = pd.DataFrame(_rows, columns=['year', 'vmt'])
+    by_year_1936_1995
+    return by_year_1936_1995,
+
+
+@app.cell
+def __(pd):
+    # Hand copied here LOL
+    _hand_copied = [
+        # [1995, 2422819], # just to check overlap
+        [1996,  2485846],
+        [1997,  2560375],
+        [1998,  2625367],
+        [1999, 2691336],
+        # [2000, 2746925] # check overlap
+    ]
+    by_year_1995_2000 = pd.DataFrame(_hand_copied, columns=['year', 'vmt'])
+    return by_year_1995_2000,
+
+
+@app.cell
+def __(pd):
+    by_month_2000_2024 = pd.read_csv(
+        'data/Vehicle miles traveled 2000-May 2024 -- fhwa - 24maytvt > SAVMT.csv',
+        thousands=','
+    )
+
+    by_month_2000_2024['year'] = by_month_2000_2024['obs_date'].apply(lambda x: '20' + x.split('-')[1])
+
+    by_year_2000_2023 = by_month_2000_2024.groupby('year').agg({'vmt': 'sum'}).reset_index()
+
+    by_year_2000_2023 = by_year_2000_2023[
+            by_year_2000_2023['year'].apply(lambda x: int(x)) < 2024
+    ]
+
+    by_year_2000_2023
+    return by_month_2000_2024, by_year_2000_2023
+
+
+@app.cell
+def __(
+    by_year_1936_1995,
+    by_year_1995_2000,
+    by_year_2000_2023,
+    pd,
+    plt,
+    sns,
+):
+    # Concat all 3 dataframes
+
+    all_years = pd.concat([by_year_1936_1995, by_year_1995_2000, by_year_2000_2023])
+    all_years
+
+    # make a time series chart using sns
+
+    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(16, 6))
+    sns.lineplot(data=all_years, x='year', y='vmt')
+    sns.lineplot(data=all_years, x='year', y='vmt')
+    return all_years,
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        r"""
+        # B. US population by year
+
+        There's a lot of sources, not too many complete ones.
+
+        [I used St Lous Fed](https://fred.stlouisfed.org/graph/?id=B230RC0A052NBEA,) to pull full 1936-2023 range.
+        """
+    )
+    return
+
+
+@app.cell
+def __(pd):
+    annual_population = pd.read_csv("data/us_annual_population_1936_2023.csv")
+    annual_population['year'] = annual_population['date'].apply(lambda x: int(x.split('-')[0]))
+    annual_population = annual_population[['year', 'population']]
+    annual_population
+    return annual_population,
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""# C. Export miles driven per capita""")
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()

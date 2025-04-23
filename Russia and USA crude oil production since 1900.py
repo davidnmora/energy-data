d30f09d807a199771dee645aede011a0655d122c
David Mora <davidnmora@gmail.com>
2025-04-23 11:05:33 -0700

    make russia empire/ussr line continuous

--- Russia and USA crude oil production since 1900.py
+++ Russia and USA crude oil production since 1900.py
@@ -15,11 +15,6 @@ def __():
 
 
 @app.cell
-def __():
-    return
-
-
-@app.cell
 def __(mo):
     mo.md(
         r"""
@@ -65,41 +60,66 @@ def __(mo):
 
 
 @app.cell
-def __(np, raw_df, sns):
+def __(np, pd, raw_df, sns):
     df = raw_df.rename(columns={"Year": "year", "oil_production__twh": "oil_production_twh", "Entity": "country"})
 
     # Rename country between certain regions: 
-    # "USSR" 1900 - 1921 should be "Russian Empire"
+    # "USSR" 1900 - USSR_START should be "Russian Empire" (note it's inclusive, because we want the line to be continuous visually)
+    USSR_START = 1922
     df['country'] = np.where(
-        (df['country'] == 'USSR') & (df['year'] < 1922), 
+        (df['country'] == 'USSR') & (df['year'] <= USSR_START), 
         'Russian Empire', 
         df['country']
     )
-    # "USSR" should only exist 1922 - 1991, anything "USSR" after should be dropped
-    df = df[~((df['country'] == 'USSR') & (df['year'] > 1991))]
-    # "Russia" should only exist 1991 - present, anything before 1991 should be dropped
-    df = df[~((df['country'] == 'Russia') & (df['year'] < 1991))]
+    # Manually duplicate 'Russian Empire' for the years 1922 and rename it USSR:
+    df = pd.concat([
+        df,
+        df[
+            (df['country'] == 'Russian Empire') & 
+            (df['year'] == USSR_START)
+        ].assign(year=USSR_START, country='USSR')
+    ])
+
+
+    # "USSR" should only exist USSR_START - USSR_END, anything "USSR" after should be dropped
+    USSR_END = 1991
+    df = df[~((df['country'] == 'USSR') & (df['year'] > USSR_END))]
+    # "Russia" should only exist USSR_END - present, anything before USSR_END should be dropped
+    df = df[~((df['country'] == 'Russia') & (df['year'] < USSR_END))]
 
 
     sns.lineplot(data=df, x="year", y="oil_production_twh", hue="country")
-    return (df,)
+    return USSR_END, USSR_START, df
 
 
 @app.cell
 def __(df):
     # Sort the data by year
     sorted_df = df.sort_values(by="year")
-    """
-    I want to output a JSON in the format
-    {
-        usa: [] # all the usa data,
-        russianEmpire: [...],
-        ussr: [...],
-        russia: [...]
-    }
-    """
 
-    return (sorted_df,)
+    # Create dictionary with data for each country
+    output_dict = {
+        country: sorted_df[sorted_df['country'] == country].to_dict('records')
+        for country in sorted_df['country'].unique()
+    }
+
+    # Convert to JSON format, print to a file
+    import json
+    json_output = json.dumps(output_dict, indent=2)
+    return json, json_output, output_dict, sorted_df
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""# Copy json output to clipboard for export""")
+    return
+
+
+@app.cell
+def __(json_output):
+    import pyperclip
+    pyperclip.copy(json_output)
+    return (pyperclip,)
 
 
 @app.cell
